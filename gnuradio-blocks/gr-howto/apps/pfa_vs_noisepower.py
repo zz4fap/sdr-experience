dbf6d69d108f2c6f343ef19ce2d0c3890e18d13d
zz4fap <zz4fap@c12590a7-8cfc-82db-b146-8243c26d6270>
2013-01-09 21:44:13 +0000

    Add python script with simulation for Pfa vs. noise power

---
+++ gnuradio-blocks/gr-howto/apps/pfa_vs_noisepower.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+##################################################
+# Gnuradio Python Flow Graph
+# Title: Pfa vs. Noise power
+# Generated: Wed Jan  9 19:24:29 2013
+##################################################
+
+import math
+from gnuradio import eng_notation
+from gnuradio import fft
+from gnuradio import gr
+from gnuradio import window
+from gnuradio.eng_option import eng_option
+from gnuradio.gr import firdes
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import howto
+import wx
+
+def powerToAmplitude(dbW)
+   return math.log10(dbW)
